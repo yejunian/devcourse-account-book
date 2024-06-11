412e887b83933a66fa199498b70382a60f840677
yejunian <leeye51456@gmail.com>
2024-06-12 00:08:23 +0900

    Add implementation

--- js/components/history-list.js
+++ js/components/history-list.js
@@ -31,46 +31,63 @@ function reRender() {
 }
 
 export function renderHistoryList() {
-  // TODO: 데이터 매핑
-  // TODO: 오름차순으로 목록 나열
-  // TODO: 항목의 시간 포맷 변경: `HH:mm`
-  // TODO: 금액 콤마 포맷 맞추기
-
   $sectionHistory.innerHTML = store.dateList
     .map(({ date, id: dateId }) => {
-      const detail = store.detailList[dateId];
+      // sort 메서드는 원본을 변경해서 그걸 리턴하므로, 복제해서 적용함
+      const detail = [...store.detailList[dateId]].sort((a, b) => b.id - a.id);
       if (!detail?.length) return '';
 
-      return `<article class="history-per-day">
-      <p class="history-date">2021년 12월 1일</p>
-      <section class="history-item">
-        <section class="history-item-column">
-          <div class="create-at">10:30</div>
-          <div class="history-detail">
-            <div class="history-detail-row history-detail-title">
-              <p>아이스 아메리카노</p>
-            </div>
-            <div class="history-detail-row history-detail-subtitle">
-              <p>카페</p>
-              <p>
-                1000000
-                <span>원</span>
-              </p>
-            </div>
-          </div>
-          <div class="delete-section">
-            <button class="delete-button">🗑</button>
-          </div>
-        </section>
-        <section class="history-item-caption">
-          <p>
-            <span>남은 자산</span>
-            <span>300000</span>
-            <span>원</span>
-          </p>
-        </section>
-      </section>
-    </article>`;
+      return `
+        <article class="history-per-day">
+          <p class="history-date">${date}</p>
+          ${detail
+            .map(
+              ({
+                description,
+                category,
+                amount,
+                fundsAtTheTime,
+                createAt,
+                id,
+              }) => {
+                const time = new Date(createAt).toLocaleTimeString('ko', {
+                  hourCycle: 'h24',
+                  timeStyle: 'short',
+                });
+                return `
+                  <section class="history-item">
+                    <section class="history-item-column">
+                      <div class="create-at">${time}</div>
+                      <div class="history-detail">
+                        <div class="history-detail-row history-detail-title">
+                          <p>${description}</p>
+                        </div>
+                        <div class="history-detail-row history-detail-subtitle">
+                          <p>${category}</p>
+                          <p>
+                            ${amount.toLocaleString()}
+                            <span>원</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div class="delete-section">
+                        <button class="delete-button" data-dateid=${dateId} data-itemid=${id}>🗑</button>
+                      </div>
+                    </section>
+                    <section class="history-item-caption">
+                      <p>
+                        <span>남은 자산</span>
+                        <span>${fundsAtTheTime.toLocaleString()}</span>
+                        <span>원</span>
+                      </p>
+                    </section>
+                  </section>
+                `;
+              }
+            )
+            .join('')}
+        </article>
+      `;
     })
     .join('');
 }
--- js/store.js
+++ js/store.js
@@ -49,13 +49,13 @@ export function initStore() {
 
 export function addNewHistory(newHistory) {
   try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList = null;
-    store.currentFunds = null;
+    if (store.detailList[store.todayId]) {
+      store.detailList[store.todayId].push(newHistory);
+    } else {
+      store.detailList[store.todayId] = [newHistory];
+    }
+
+    store.currentFunds -= newHistory.amount;
 
     updateStorage();
     return true;
@@ -67,12 +67,14 @@ export function addNewHistory(newHistory) {
 
 export function removeHistory(dateId, itemId) {
   try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList[dateId] = null;
+    store.detailList[dateId] = store.detailList[dateId].filter(
+      ({ id, amount }) => {
+        if (id === Number(itemId)) {
+          store.currentFunds += amount;
+        }
+        return id !== Number(itemId);
+      }
+    );
 
     updateStorage();
     return true;
