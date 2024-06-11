d7b6470bfa20a65c90b95ba2fbf9400e79e0cb28
yejunian <leeye51456@gmail.com>
2024-06-11 23:14:02 +0900

    Initial commit

---
+++ js/components/history-list.js
@@ -0,0 +1,76 @@
+import { renderCurrentAsset } from '../components/current-asset';
+import { store, removeHistory } from '../store';
+
+const $sectionHistory = document.querySelector('.history');
+
+export function initHistoryList() {
+  renderHistoryList();
+  addHistoryListEventListener();
+}
+
+function addHistoryListEventListener() {
+  $sectionHistory.addEventListener('click', function (event) {
+    const element = event.target;
+    if (!element.className.includes('delete-button')) return;
+
+    const { dateid, itemid } = element.dataset;
+
+    const isSuccess = removeHistory(dateid, itemid);
+    if (!isSuccess) {
+      alert('소비내역 삭제에 실패했습니다.');
+      return;
+    }
+
+    reRender();
+  });
+}
+
+function reRender() {
+  renderCurrentAsset();
+  renderHistoryList();
+}
+
+export function renderHistoryList() {
+  // TODO: 데이터 매핑
+  // TODO: 오름차순으로 목록 나열
+  // TODO: 항목의 시간 포맷 변경: `HH:mm`
+  // TODO: 금액 콤마 포맷 맞추기
+
+  $sectionHistory.innerHTML = store.dateList
+    .map(({ date, id: dateId }) => {
+      const detail = store.detailList[dateId];
+      if (!detail?.length) return '';
+
+      return `<article class="history-per-day">
+      <p class="history-date">2021년 12월 1일</p>
+      <section class="history-item">
+        <section class="history-item-column">
+          <div class="create-at">10:30</div>
+          <div class="history-detail">
+            <div class="history-detail-row history-detail-title">
+              <p>아이스 아메리카노</p>
+            </div>
+            <div class="history-detail-row history-detail-subtitle">
+              <p>카페</p>
+              <p>
+                1000000
+                <span>원</span>
+              </p>
+            </div>
+          </div>
+          <div class="delete-section">
+            <button class="delete-button">🗑</button>
+          </div>
+        </section>
+        <section class="history-item-caption">
+          <p>
+            <span>남은 자산</span>
+            <span>300000</span>
+            <span>원</span>
+          </p>
+        </section>
+      </section>
+    </article>`;
+    })
+    .join('');
+}
---
+++ js/store.js
@@ -0,0 +1,83 @@
+/**
+ *  dateList {
+      date: new Date("2000-01-10").toLocaleDateString(),
+      id: "2",
+    }[]
+ *  detailList {
+      2: {
+        id: Date.now() + 1000,
+        createAt: new Date(),
+        description: "삼겹살",
+        category: "식사",
+        amount: 20000,
+        fundsAtTheTime: 9978000,
+      }[]
+    }
+ */
+export const store = {
+  currentFunds: 0,
+
+  isFirstEdit: true,
+  todayId: 1,
+
+  dateList: [
+    {
+      id: 1,
+      date: new Date().toLocaleDateString(),
+    },
+  ],
+  detailList: {},
+};
+
+export function updateStorage() {
+  sessionStorage.setItem('store', JSON.stringify(store));
+}
+
+export function initStore() {
+  const storage = sessionStorage.getItem('store');
+  if (!storage) updateStorage();
+
+  const { dateList, detailList, todayId, currentFunds, isFirstEdit } =
+    JSON.parse(storage);
+
+  store.currentFunds = currentFunds;
+  store.isFirstEdit = isFirstEdit;
+  store.dateList = dateList;
+  store.detailList = detailList;
+  store.todayId = todayId;
+}
+
+export function addNewHistory(newHistory) {
+  try {
+    // TODO:
+    /**
+     * - store의 detailList 새로 갱신
+     * - store.currentFunds 새로 갱신
+     */
+    store.detailList = null;
+    store.currentFunds = null;
+
+    updateStorage();
+    return true;
+  } catch (error) {
+    alert(error);
+    return false;
+  }
+}
+
+export function removeHistory(dateId, itemId) {
+  try {
+    // TODO:
+    /**
+     * - store의 detailList 새로 갱신
+     * - store.currentFunds 새로 갱신
+     */
+    store.detailList[dateId] = null;
+
+    updateStorage();
+    return true;
+  } catch (error) {
+    alert(error);
+    return false;
+  }
+}
